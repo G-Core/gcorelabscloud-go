5937eb429e1759e123450a38a2b728b593282602
Danil Krox <deferred@users.noreply.github.com>
2025-02-20 18:03:25 +0100

    Implement list clusters logic

--- gcore/gpu/v3/clusters/urls.go
+++ gcore/gpu/v3/clusters/urls.go
@@ -6,6 +6,11 @@ const (
 	clustersPath = "clusters"
 )
 
+// ClustersURL returns URL for GPU clusters operations
+func ClustersURL(c *gcorecloud.ServiceClient) string {
+	return c.ServiceURL(clustersPath)
+}
+
 // ClusterURL returns URL for specific GPU cluster operations
 func ClusterURL(c *gcorecloud.ServiceClient, clusterID string) string {
 	return c.ServiceURL(clustersPath, clusterID)
