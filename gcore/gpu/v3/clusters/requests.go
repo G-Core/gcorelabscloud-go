256bc1d2479136ebea84d55c1f9e0587dd127898
Danil Krox <deferred@users.noreply.github.com>
2025-02-20 18:03:25 +0100

    Implement list clusters logic

--- gcore/gpu/v3/clusters/requests.go
+++ gcore/gpu/v3/clusters/requests.go
@@ -3,6 +3,7 @@ package clusters
 import (
 	gcorecloud "github.com/G-Core/gcorelabscloud-go"
 	"github.com/G-Core/gcorelabscloud-go/gcore/task/v1/tasks"
+	"github.com/G-Core/gcorelabscloud-go/pagination"
 	"net/http"
 )
 
@@ -27,6 +28,44 @@ func (opts RenameClusterOpts) ToRenameClusterActionMap() (map[string]interface{}
 		return nil, err
 	}
 	return gcorecloud.BuildRequestBody(opts, "")
+}
+
+// ListClustersOptsBuilder allows extensions to add additional parameters to the List request.
+type ListClustersOptsBuilder interface {
+	ToListClustersQuery() (string, error)
+}
+
+// ListOpts allows the filtering and sorting of paginated collections through the API.
+type ListOpts struct {
+	Limit  int `q:"limit" validate:"omitempty,gt=0"`
+	Offset int `q:"offset" validate:"omitempty,gt=0"`
+}
+
+// ToListClustersQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToListClustersQuery() (string, error) {
+	if err := gcorecloud.ValidateStruct(opts); err != nil {
+		return "", err
+	}
+	q, err := gcorecloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), err
+}
+
+// List retrieves list of GPU flavors
+func List(client *gcorecloud.ServiceClient, opts ListClustersOptsBuilder) pagination.Pager {
+	url := ClustersURL(client)
+	if opts != nil {
+		query, err := opts.ToListClustersQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return ClusterPage{pagination.LinkedPageBase{PageResult: r}}
+	})
 }
 
 type ServerCredentialsOpts struct {
