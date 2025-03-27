af3c6bcf09278015b8ebb147d7fdf833c3043bcf
Pedro Oliveira <pedro.oliveira@gcore.com>
2025-03-27 16:04:46 +0000

    Add v3 gpu cluster delete method

--- gcore/gpu/v3/clusters/requests.go
+++ gcore/gpu/v3/clusters/requests.go
@@ -2,6 +2,7 @@ package clusters
 
 import (
 	gcorecloud "github.com/G-Core/gcorelabscloud-go"
+	"github.com/G-Core/gcorelabscloud-go/gcore/task/v1/tasks"
 	"net/http"
 )
 
@@ -32,6 +33,13 @@ func (opts RenameClusterOpts) ToRenameClusterActionMap() (map[string]interface{}
 func Get(client *gcorecloud.ServiceClient, clusterID string) (r GetResult) {
 	url := ClusterURL(client, clusterID)
 	_, r.Err = client.Get(url, &r.Body, nil)
+	return
+}
+
+// Delete removes a specific GPU cluster by its ID.
+func Delete(client *gcorecloud.ServiceClient, clusterID string) (r tasks.Result) {
+	url := ClusterURL(client, clusterID)
+	_, r.Err = client.DeleteWithResponse(url, &r.Body, nil) // nolint
 	return
 }
 
