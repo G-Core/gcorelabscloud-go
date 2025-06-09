ed01e3bc7de7aae3167c1a24dd6c14003101fc0b
Grzegorz Bernady <grzegorz.bernady@gcore.com>
2025-06-09 11:25:41 +0200

    MKAAS-1518 Use pager when listing gpu cluster servers, allow deleting them (#203)

--- gcore/gpu/v3/servers/results.go
+++ gcore/gpu/v3/servers/results.go
@@ -2,34 +2,43 @@ package servers
 
 import (
 	gcorecloud "github.com/G-Core/gcorelabscloud-go"
+	"github.com/G-Core/gcorelabscloud-go/pagination"
 )
 
-// ListResult represents the result of a List operation.
-type ListResult struct {
-	gcorecloud.Result
+// ServerPage is the page returned by a pager when traversing over a collection of servers.
+type ServerPage struct {
+	pagination.LinkedPageBase
 }
 
-// Extract is a function that accepts a result and extracts a list of servers.
-func (r ListResult) Extract() (*ServersPage, error) {
-	var s ServersPage
+// NextPageURL is invoked when a paginated collection of AI Clusters has reached
+// the end of a page and the pager seeks to traverse over a new one. In order
+// to do this, it needs to construct the next page's URL.
+func (r ServerPage) NextPageURL() (string, error) {
+	var s struct {
+		Links []gcorecloud.Link `json:"links"`
+	}
 	err := r.ExtractInto(&s)
-	return &s, err
+	if err != nil {
+		return "", err
+	}
+	return gcorecloud.ExtractNextURL(s.Links)
 }
 
-// ServersPage represents a page of servers results.
-type ServersPage struct {
-	Count   int      `json:"count"`
-	Results []Server `json:"results"`
+// IsEmpty checks whether a ServerPage struct is empty.
+func (r ServerPage) IsEmpty() (bool, error) {
+	is, err := ExtractServers(r)
+	return len(is) == 0, err
 }
 
-// IsEmpty returns true if a ServersPage contains no results.
-func (r ServersPage) IsEmpty() bool {
-	return len(r.Results) == 0
+// ExtractServers accepts a Page struct, specifically a ServerPage struct,
+// and extracts the elements into a slice of Server structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractServers(r pagination.Page) ([]Server, error) {
+	var s []Server
+	err := ExtractServersInto(r, &s)
+	return s, err
 }
 
-// ExtractServers accepts a ListResult and extracts the servers.
-func ExtractServers(lr ListResult) ([]Server, error) {
-	var s ServersPage
-	err := lr.ExtractInto(&s)
-	return s.Results, err
+func ExtractServersInto(r pagination.Page, v interface{}) error {
+	return r.(ServerPage).Result.ExtractIntoSlicePtr(v, "results")
 }
